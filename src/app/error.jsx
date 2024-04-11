9393d293843e3faaed0055c3aaf282eabd20127a
nguyentuannguyenan <nguyenan29072001@gmail.com>
2024-04-11 09:58:59 +0700

    fixed

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -1,10 +1,9 @@
 'use client'
 import React from 'react'
-import { useState, useEffect } from 'react'
+import { useEffect } from 'react'
 
 export default function error({error, reset}) {
     useEffect(() => {
-
         console.log(error);
     }, [error]);
   return (

729d75220f16b5ce8ad0f1f9a2b6dc023e4f3030
nguyentuannguyenan <nguyenan29072001@gmail.com>
2024-04-17 10:56:16 +0700

    bugs fixed

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function loading() {
   return (
     <div className="flex justify-center">
-      <Image className="h-50 mt-15" src="spinner.svg" alt="loading..." />
+      <Image className="h-50 mt-15" src="spinner.svg" alt="loading..." width={200} height={200}/>
     </div>
   )
 }
